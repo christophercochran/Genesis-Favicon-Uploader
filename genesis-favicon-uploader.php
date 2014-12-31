e1f2e33a6bad1bb0dc5f9ab256e0c7a2ea999dd4
Christopher Cochran <chris@maintainn.com>
2014-12-31 18:29:39 -0500

    0.1.2

--- genesis-favicon-uploader.php
+++ genesis-favicon-uploader.php
@@ -3,7 +3,7 @@
 Plugin Name: Genesis Favicon uploader
 Plugin URI: http://genesistutorials.com/plug-ins/genesis-favicon-uploader/
 Description: Upload your own favicon!
-Version: .1.1b
+Version: 0.1.2
 Author: Christopher Cochran
 Author URI: http://christophercochran.me
 */
